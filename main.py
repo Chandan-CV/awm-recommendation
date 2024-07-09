8b2b1299d4d252504391fc848cb4a63a9f7e6384
Chandan <chandancvp08@gmail.com>
2024-07-09 08:55:30 +0530

    simplified the model a lott

--- main.py
+++ main.py
@@ -2,23 +2,24 @@ import joblib
 import numpy as np
 import pandas as pd
 import streamlit as st
-from preprocessing import get_features_array
+from preprocessing import get_features_array, preprocessData
 dataurl = 'https://docs.google.com/spreadsheets/d/1EmeVCJzCiMISggPuS5clv-gz4mznq24NMyFV8aXkbK4/edit?gid=0#gid=0'
 dataurl = dataurl.replace('/edit?gid=', '/export?format=csv&gid=')
 dataset = pd.read_csv(dataurl)
 # dataset = dataset[dataset['My age'] != 'test']
 dataset = dataset.fillna('')
 
-model = joblib.load('v1_knn.joblib')
+model = joblib.load('simpleclassifier.joblib')
 
 
-features_array = get_features_array(dataset)
-st.title("AndWeMet")
+features_array = preprocessData(dataset)
+st.title("andwemet")
 
+st.write("All users:")
 DatasetSlot = st.empty()
 rowNumberSlot = st.empty()
 findMyMatchSlot = st.empty()
-showMyProfileSlot = st.empty()
+# showMyProfileSlot = st.empty()
 DisplayMatchesSlot = st.empty()
 
 
@@ -30,30 +31,13 @@ if "matches" not in st.session_state:
 if st.session_state.matches is not None:
     DisplayMatchesSlot.write(st.session_state.matches)
 
-# def find_match():
-#     global row
-#     global slot4
-#     features = features_array.getrow(row)
-#     features = features.reshape(1, -1)
-#     distances, indices = model.kneighbors(features)
-#     match_index = indices[0]
-#     data = dataset.iloc[match_index]
-#     data = data[data["My Gender"] != dataset.iloc[row]["My Gender"]]
-#     slot4.write(data)
+row = rowNumberSlot.number_input("Enter your row number and press enter", min_value=0, max_value=len(dataset)-1, step=1)
+findMyMatchSlot.button(f"Find match for row: {row}", on_click=lambda: find_match(row))
+# showMyProfileSlot.write(dataset.iloc[row].astype(str))
 
 
-# row = slot2.text_input("Enter the row number")
-# if(row != ''):
-#     row = int(row)
-#     rowToDisplay = dataset.iloc[row].astype(str)
-#     slot3.button("Find the match", on_click=find_match)
-#     slot4.dataframe(rowToDisplay)
-
-row = rowNumberSlot.number_input("Enter the row number", min_value=0, max_value=len(dataset)-1, step=1)
-findMyMatchSlot.button("Find my match", on_click=lambda: find_match(row))
-showMyProfileSlot.write(dataset.iloc[row].astype(str))
 def find_match(row):
-    features = features_array.getrow(row)
+    features = features_array[row]
     features = features.reshape(1, -1)
     distances, indices = model.kneighbors(features)
     match_index = indices[0]
--- preprocessing.py
+++ preprocessing.py
@@ -2,11 +2,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
 from scipy.sparse import hstack
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-
+import pandas as pd
+import numpy as np
 
 
 
 def get_features_array(data):
+
     text_columns = ["I define privilege to be (having a supportive family, dependable friends, a job you love, anything else)",
     "About my growing up years  (they were awesome, they were challenging but all is good now, about the family, about friends, memories etc etc)",
     "Anything else that you may like to mention to the community members (any specifics which may be important to you)"]
@@ -15,6 +17,12 @@ def get_features_array(data):
                         "Name of the institute I graduated from last.", "Educational degree",
                         'Ball park of my professional annual income']
     numerical_columns = ['My age']
+
+
+    age_weight = 3.0
+    relationship_status = 2.0
+    city_weight = 1.5
+
 
     # Vectorize text columns using TF-IDF
     tfidf_vectorizers = {}
@@ -26,7 +34,6 @@ def get_features_array(data):
         tfidf_vectorizers[column] = vectorizer
         tfidf_features.append(tfidf_matrix)
 
-    # Encode categorical variables using LabelEncoder
     label_encoders = {}
     encoded_categorical_features = []
 
@@ -34,13 +41,43 @@ def get_features_array(data):
         le = LabelEncoder()
         encoded_column = le.fit_transform(data[column])
         label_encoders[column] = le
-        encoded_categorical_features.append(encoded_column.reshape(-1, 1))
+        if(column == 'My city of residence'):
+            encoded_categorical_features.append(encoded_column.reshape(-1, 1)*city_weight)
+        elif(column == 'My current relationship status'):
+            encoded_categorical_features.append(encoded_column.reshape(-1, 1)*relationship_status)
+        else:
+            encoded_categorical_features.append(encoded_column.reshape(-1, 1))
 
-    # Standardize numerical features
+
     scaler = StandardScaler()
     scaled_numerical_features = scaler.fit_transform(data[numerical_columns])
+    scaled_numerical_features[:, 0] *= age_weight
 
     # Combine all features into a single feature matrix
     features = hstack(tfidf_features + encoded_categorical_features + [scaled_numerical_features])
 
+    return features
+
+
+
+def preprocessData(dataset):
+    dataset = (dataset.iloc[:,:3]).join(dataset.iloc[:,5])
+
+    vectorizer = TfidfVectorizer()
+    city_vectorized = vectorizer.fit_transform(dataset['My city of residence'])
+
+    # one hot encoding
+    encoded_categorical_features =  (pd.get_dummies(dataset['My current relationship status'], dtype=int))
+
+    #label encode
+    labelencoder = LabelEncoder()
+    labeledFeatures = labelencoder.fit_transform(dataset['My Gender'])
+
+    scaler = StandardScaler()
+    scaled_numerical_features= scaler.fit_transform((dataset.iloc[:,1].values).reshape(-1,1))
+
+    labeledFeatures=labeledFeatures.reshape(-1,1)
+    city_vectorized = city_vectorized.toarray()
+
+    features = np.hstack([labeledFeatures, scaled_numerical_features, encoded_categorical_features.values, city_vectorized])
     return features
